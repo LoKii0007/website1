a2c6355a987349de6f0e3a552733584481df4aec
LoKii0007 <107889670+LoKii0007@users.noreply.github.com>
2023-10-17 17:37:02 +0530

    Update settings.py

--- raobuilders/settings.py
+++ raobuilders/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jyg21uib9a*yvvqq@hfgg3$%s+@6q5w)p_@_*zndgn$dh3hzxa'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
